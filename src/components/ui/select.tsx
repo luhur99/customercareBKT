60c6e54a9027178c9223267cddcf9249ab5a4f45
[elmony] <luhur@budikaryateknologi.com>
2026-01-19 00:35:48 +0700

    [elmony] Mengembalikan SelectTrigger ke pola shadcn/ui standar - wrote 1 file(s)

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -22,11 +22,12 @@ const SelectTrigger = React.forwardRef<
     )}
     {...props}
   >
-    {/* Membungkus children dan ChevronDown dalam satu div */}
-    <div className="flex items-center justify-between w-full">
-      {children}
+    {/* Menggunakan SelectPrimitive.Value dengan asChild untuk merender children */}
+    <SelectPrimitive.Value asChild>{children}</SelectPrimitive.Value>
+    {/* Menggunakan SelectPrimitive.Icon dengan asChild untuk ikon */}
+    <SelectPrimitive.Icon asChild>
       <ChevronDown className="h-4 w-4 opacity-50" />
-    </div>
+    </SelectPrimitive.Icon>
   </SelectPrimitive.Trigger>
 ));
 SelectTrigger.displayName = SelectPrimitive.Trigger.displayName;
